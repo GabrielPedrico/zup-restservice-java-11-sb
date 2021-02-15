76a66cf27bb1756ca0055f5558c783790ecabbc0
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-02-15 16:17:43 -0300

    Projeto Criado

---
+++ src/main/java/com/vacinaweb/zuprestservice/ZuprestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vacinaweb.zuprestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuprestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuprestserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacinaweb/zuprestservice/ZuprestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacinaweb.zuprestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuprestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
